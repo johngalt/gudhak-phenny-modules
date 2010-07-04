ad2584f9556c50ca7298ac27819f284637dde139
John Galt <whosatlas@gmail.com>
2010-07-04 17:53:13 -0500

    added armory.py, wow.py, and tv.py

---
+++ tv.py
@@ -0,0 +1,103 @@
+import urllib2
+
+def tv(phenny, input):
+	show = input.split(' ')[1:]
+	show = ' '.join(show)
+	if not show:
+		phenny.reply('Sorry, you did not supply a show name')
+	else:
+		show = show.replace(' ', '%20')
+		html = urllib2.urlopen('http://services.tvrage.com/tools/quickinfo.php?show=' + show).read()
+		data = html.split('\n')
+		z = 0
+		
+		for x in data:
+			y = x.split('@')
+			if y[0] == 'Show Name':
+				showName = y[1]
+			elif y[0] == 'Premiered':
+				showPremiered = y[1]
+			elif y[0] == 'Started':
+				showStarted = y[1]
+			elif y[0] == 'Ended':
+				if y[1]:
+					showEnded = y[1]
+				else:
+					showEnded = 'Still Running'
+			elif y[0] == 'Country':
+				showCountry = y[1]
+			elif y[0] == 'Genres':
+				showGenres = y[1]
+			elif y[0] == 'Network':
+				showNetwork = y[1]
+			elif y[0] == 'Airtime':
+				showAirTime = y[1]
+			elif y[0] == 'Runtime':
+				showRunTime = y[1]
+	
+		phenny.say('\002TV -->\002 \037Show Name:\037 ' + showName + ' | \037Premiered:\037 ' + showPremiered + ' | \037Started:\037 ' + showStarted + ' | \037Ended:\037 ' + showEnded + ' | \037Country:\037 ' + showCountry)
+		phenny.say('\037Genres:\037 ' + ', '.join(showGenres.split('|')) + ' | \037Network:\037 ' + showNetwork + ' | \037Airtime:\037 ' + showAirTime)
+		
+tv.commands = ['tv']
+tv.example = '.tv House'
+
+def next(phenny, input):
+    show = input.split(' ')[1:]
+    show = ' '.join(show)
+    if not show:
+        phenny.reply('Sorry, you did not supply a show name')
+    else:
+        show = show.replace(' ', '%20')
+        html = urllib2.urlopen('http://services.tvrage.com/tools/quickinfo.php?show=' + show).read()
+        data = html.split('\n')
+        z = 0
+
+        for x in data:
+            y = x.split('@')
+            if y[0] == 'Show Name':
+                showName = y[1]
+            elif y[0] == 'Next Episode':
+                showNextRaw =  y[1]
+
+        showNext = showNextRaw.split('^')
+
+        if not showName:
+            phenny.say('Could not find tv show with that name')
+        elif not showNext[0]:
+            phenny.say('Next episode is not available')
+        else:
+            phenny.say('\002Next -->\002 The next episode of\037 ' + showName + '\037 is \037' + showNext[1] + '\037 (' + showNext[0] + ') on ' + showNext[2])
+
+next.commands = ['next']
+next.example = '.next House'
+
+def last(phenny, input):
+    show = input.split(' ')[1:]
+    show = ' '.join(show)
+    if not show:
+        phenny.reply('Sorry, you did not supply a show name')
+    else:
+        show = show.replace(' ', '%20')
+        html = urllib2.urlopen('http://services.tvrage.com/tools/quickinfo.php?show=' + show).read()
+        data = html.split('\n')
+        z = 0
+
+        for x in data:
+            y = x.split('@')
+            if y[0] == 'Show Name':
+                showName = y[1]
+            elif y[0] == 'Latest Episode':
+                showLastRaw =  y[1]
+
+        showLast = showLastRaw.split('^')
+
+        if not showName:
+            phenny.say('Could not find tv show with that name')
+        elif not showLast[0]:
+            phenny.say('Last episode of not available')
+        else:
+            phenny.say('\002Last -->\002 The last episode of\037 ' + showName + '\037 was \037' + showLast[1] + '\037 (' + showLast[0] + ') on ' + showLast[2])
+
+last.commands = ['last']
+last.example = '.last House'
+
---
+++ wow.py
@@ -0,0 +1,44 @@
+from armory import Armory
+from xml.parsers.expat import ExpatError
+
+def wow(phenny, input):
+	if not input:
+		# return 'Invalid Syntax. Proper syntax: .wow Name @Realm @US/EU'
+		phenny.say('Invalid Syntax. Proper syntax: .wow Name @Realm @US/EU')
+	else:
+		input = input.split(' ')
+		input = ' '.join(input[1:])
+		input = input.split('@')
+		if input[0]:
+			charName = input[0].upper()
+		if input[1]:
+			charRealm = input[1].rstrip()
+		if input[2]:
+			charRegion = input[2]
+		if not input[0]:
+			phenny.say('Invalid Syntax. Proper syntax: .wow Name @Realm @US/EU')
+		else:
+			# return 'Char Name: ' + charName + ' | Char Realm: ' + charRealm + ' | Char Region: ' + charRegion
+			try:
+				char = Armory().getCharacter(charName, charRealm, charRegion)
+			except ExpatError:
+				# return 'Error in retrieving character information'
+				phenny.say('Error in retrieving character information')
+				return
+			except IndexError:
+				phenny.say('Error in retrieving character information')
+				return
+			charTalents = '/'.join(map(str, char['talents']))
+			charProf = ''
+			for x in char['professions']:
+				try:
+					charProf += '%s: \00307%s\003 ' % (x['name'], x['value'])
+				except KeyError:
+					charProf += "No Skill "
+					
+			phenny.say('*** [\00307 %s \003]: \00307%s %s %s\003 (\00309%s\003) | Guild:\00307 %s \003 | Armory: \00307<blank>\003' % (charName, char['level'], char['race'], char['class'], charTalents, char['guild']))
+			phenny.say('*** [\00307 %s \003]: H: \00307%s\003 %s: \00307%s\003 | %s | HKs:\00307 %s\003 | Resil:\00307 %s\003 | Achievs:\00307 %s\003' % (charName, char['bars']['health'], char['bars']['secondBar']['type'].upper(), char['bars']['secondBar']['effective'], charProf, char['lifetimehonorablekills'], char['defenses']['resilience']['value'], char['points']))
+			phenny.say('*** [\00307 %s \003]: Agility: \00307%s\003 | Strength: \00307%s\003 | Armor: \00307%s\003 | Spirit: \00307%s\003 | Stamina: \00307%s\003 | Intellect: \00307%s\003 | Spell Power: \00307%s\003 | Haste: \00307%s\003 | Spell Hit: \00307%s\003 | Melee Hit: \00307%s\003 | Ranged Hit: \00307%s\003' % (char['class'].upper(), char['stats']['agility'], char['stats']['strength'], char['stats']['armor'], char['stats']['spirit'], char['stats']['stamina'], char['stats']['intellect'], char['spell']['bonusHealing'], char['spell']['hasteRating']['hasteRating'], char['spell']['hitRating']['value'], char['mele']['hitRating']['value'], char['ranged']['hitRating']['value']))
+
+wow.commands = ['wow']
+wow.example = '.wow Taggart @Lightning\'s Blade @US'
